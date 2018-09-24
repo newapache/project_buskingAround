791af0fc144e5a35e6ac04237106118f9e7b7387
고희경 <>
2018-09-24 21:39:49 +0900

    g

--- app/src/main/java/finalreport/mobile/dduwcom/myapplication/Main/MainActivity.java
+++ app/src/main/java/finalreport/mobile/dduwcom/myapplication/Main/MainActivity.java
@@ -1,13 +1,19 @@
 package finalreport.mobile.dduwcom.myapplication.Main;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.MediaMetadataRetriever;
 import android.media.ThumbnailUtils;
 import android.os.Build;
 import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -23,6 +29,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -31,6 +39,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 
 import finalreport.mobile.dduwcom.myapplication.Map.BuskingMap;
@@ -48,7 +58,7 @@ import static io.antmedia.android.LiveMainActivity.RTMP_BASE_URL;
 import static io.antmedia.android.liveVideoBroadcaster.R.drawable.ic_heart_white;
 import static io.antmedia.android.liveVideoBroadcaster.R.mipmap.ic_launcher;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
 
     FirebaseAuth auth;
@@ -69,6 +79,11 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Stream> onAirposts = new ArrayList<Stream>();
     //팔로우포스트
     ArrayList<PostPromote> followingposts = new ArrayList<PostPromote>();
+    //거리포스트
+    ArrayList<PostPromote> nearbyposts = new ArrayList<PostPromote>();
+
+    LocationManager locationManager;
+    String bestProvider;
 
     private int MAX_ITEM_COUNT = 50;
 
@@ -76,8 +91,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         auth = FirebaseAuth.getInstance();
 
@@ -94,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
 
         getOnAirPost();
         getFollowingPost();
+        getNearbyPost();
 
         mLayoutManager1 = new LinearLayoutManager(this);
         mLayoutManager1.setOrientation(LinearLayoutManager.HORIZONTAL); // 기본값이 VERTICAL
@@ -104,30 +118,28 @@ public class MainActivity extends AppCompatActivity {
         mLayoutManager2.setOrientation(LinearLayoutManager.HORIZONTAL);
         mLayoutManager2.setReverseLayout(true);
         mLayoutManager2.setStackFromEnd(true);
-//
-//        mLayoutManager3 = new LinearLayoutManager(this);
-//        mLayoutManager3.setOrientation(LinearLayoutManager.HORIZONTAL);
+
+        mLayoutManager3 = new LinearLayoutManager(this);
+        mLayoutManager3.setOrientation(LinearLayoutManager.HORIZONTAL);
 
         // setLayoutManager
         mHorizontalView1.setLayoutManager(mLayoutManager1);
-
         mHorizontalView2.setLayoutManager(mLayoutManager2);
-
-//        mHorizontalView3.setLayoutManager(mLayoutManager3);
+        mHorizontalView3.setLayoutManager(mLayoutManager3);
 
         // init Adapter
         mAdapter1 = new onAirAdapter();
         mAdapter2 = new HorizontalAdapter();
-//        mAdapter3 = new HorizontalAdapter();
+        mAdapter3 = new HorizontalAdapter();
         // set Data
         mAdapter1.setData(onAirposts);
         mAdapter2.setData(followingposts);
-//        mAdapter3.setData(data3);
+        mAdapter3.setData(nearbyposts);
 
         // set Adapter
         mHorizontalView1.setAdapter(mAdapter1);
         mHorizontalView2.setAdapter(mAdapter2);
-//        mHorizontalView3.setAdapter(mAdapter3);
+        mHorizontalView3.setAdapter(mAdapter3);
 
     }
 
@@ -140,19 +152,19 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case  R.id.gomypage:
                 final Intent intent2 = new Intent(this, MypageActivity.class);
-               FirebaseDatabase.getInstance().getReference("users")
+                FirebaseDatabase.getInstance().getReference("users")
                         .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
                         .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        intent2.putExtra("user", dataSnapshot.getValue(UserModel.class));
-                        startActivity(intent2);
-                    }
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                intent2.putExtra("user", dataSnapshot.getValue(UserModel.class));
+                                startActivity(intent2);
+                            }
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
 
-                    }
-                });
+                            }
+                        });
                 break;
             case R.id.gosearchActivity:
                 Intent intent3 =  new Intent(this, SearchActivity.class);
@@ -215,6 +227,124 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void getNearbyPost() {
+        FirebaseDatabase.getInstance().getReference("post_promote").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                nearbyposts.clear();
+
+                Log.d("getNearbyPost: ",  "");
+                LatLng currentPosition = getCurrentPosition();
+                Location mCurrentLocation = new Location(LocationManager.GPS_PROVIDER);
+                mCurrentLocation.setLatitude(currentPosition.latitude);
+                mCurrentLocation.setLongitude(currentPosition.longitude);
+
+                Log.d("getNearbyPost: ", "lat=" + mCurrentLocation.getLatitude() + "lon=" + mCurrentLocation.getLongitude());
+
+                for (DataSnapshot postPrmtData : dataSnapshot.getChildren()) {
+
+                    PostPromote postPrmt = postPrmtData.getValue(PostPromote.class);
+
+                    Location mBuskingLocation = new Location(LocationManager.GPS_PROVIDER);
+                    mBuskingLocation.setLatitude(postPrmt.postPrmt_busking_latitude);
+                    mBuskingLocation.setLongitude(postPrmt.postPrmt_busking_longitude);
+                    LatLng buskingPosition = new LatLng(postPrmt.postPrmt_busking_latitude, postPrmt.postPrmt_busking_longitude);
+
+                    PolylineOptions dist = new PolylineOptions()
+                            .add(currentPosition, buskingPosition);
+                    postPrmt.setPostPrmt_distance(mCurrentLocation.distanceTo(mBuskingLocation));
+
+                    nearbyposts.add(postPrmt);
+
+                    Log.d("getNearbyPost: ", postPrmt.postPrmt_title + " : " + postPrmt.postPrmt_distance);
+                }
+                mAdapter3.notifyDataSetChanged();
+
+                Log.d("getNearbyPost: ", "distSort");
+                Comparator<PostPromote> distSort = new Comparator<PostPromote>() {
+                    @Override
+                    public int compare(PostPromote p1, PostPromote p2) {
+                        if (p1.postPrmt_distance < p2.postPrmt_distance)
+                            return -1;
+                        else if (p1.postPrmt_distance == p2.postPrmt_distance)
+                            return 0;
+                        else
+                            return 1;
+                    }
+                };
+                Collections.sort(nearbyposts, distSort);
+                mAdapter3.notifyDataSetChanged();
+
+                for (PostPromote postPrmtList : nearbyposts) {
+                    Log.d("getNearbyPost: ", postPrmtList.postPrmt_title + " : " + postPrmtList.postPrmt_distance);
+                }
+
+                onStop();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public LatLng getCurrentPosition()
+    {
+        Log.d("getCurPos: ", "");
+        ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        bestProvider = locationManager.getBestProvider(criteria, true);
+        if (bestProvider == null) {
+            Log.e( "getCurPos: ", "No location provider found!" );
+        }
+        Location location = locationManager.getLastKnownLocation(bestProvider);
+        Double lat,lon;
+        try {
+            lat = location.getLatitude ();
+            lon = location.getLongitude ();
+            Log.d("getCurPos: ", "curLat=" + lat + " curLon=" + lon);
+            return new LatLng(lat, lon);
+        }
+        catch (NullPointerException e){
+            e.printStackTrace();
+            Log.d("getCurPos: ", "error");
+            return null;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // TODO Auto-generated method stub
+
+        int latitude = (int) (location.getLatitude());
+        int longitude = (int) (location.getLongitude());
+
+        Log.i("Geo_Location", "Latitude: " + latitude + ", Longitude: " + longitude);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+
     public void getOnAirPost(){
         FirebaseDatabase.getInstance().getReference("stream").addValueEventListener(new ValueEventListener() {
             @Override
@@ -223,7 +353,7 @@ public class MainActivity extends AppCompatActivity {
                 for (DataSnapshot messageData : dataSnapshot.getChildren()) {
                     // child 내에 있는 데이터만큼 반복합니다.
 
-                  for(DataSnapshot data : messageData.getChildren()) {
+                    for(DataSnapshot data : messageData.getChildren()) {
                         Stream stream = data.getValue(Stream.class);
                         if(stream.isStreaming){
                             Log.d("Stream", stream.streamName);
@@ -288,14 +418,14 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
             try{
-            holder.icon.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent =  new Intent(MainActivity.this , streamPlayerActivity.class);
-                    intent.putExtra("streamName", data.getStreamName()+ " live=1");
-                    startActivity(intent);
-                }
-            });}catch (Exception e){
+                holder.icon.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent =  new Intent(MainActivity.this , streamPlayerActivity.class);
+                        intent.putExtra("streamName", data.getStreamName()+ " live=1");
+                        startActivity(intent);
+                    }
+                });}catch (Exception e){
                 Toast.makeText(MainActivity.this, "방송연결중 오류가 났습니다.", Toast.LENGTH_SHORT).show();
             }
 
